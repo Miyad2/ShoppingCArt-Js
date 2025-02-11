5144450205ecace1c590db357c06709035e7a0d9
Miyad <miyadmursalinrimu@gmail.com>
2025-02-11 14:27:44 +0600

    total price

--- shoppingCart.js
+++ shoppingCart.js
@@ -26,6 +26,8 @@ var bookThree = {
 var productArray = [bookOne, 5, bookTwo, 3, bookThree, 1]
 
 var count = 1
+var totalQty=0
+var TotalPrice=0
 
 for (let i = 0; i < productArray.length; i+=2) {
     var ProductName = productArray[i].name;
@@ -37,8 +39,13 @@ for (let i = 0; i < productArray.length; i+=2) {
     var totalDiscount=productArray[i].price * ((productArray[i].discount*productCount) / 100);
     var priceAfterDiscount = totalPrice - totalDiscount;
 
-    console.log(`${count}. Product Name: ${ProductName}, Price: ${BasePrice}, reviews: ${reviews}, Rating: ${rating}, Total Discount: ${totalDiscount}, Total Price: ${priceAfterDiscount}`)
+    console.log(`${count}. Product Name: ${ProductName}, Price: ${BasePrice}, reviews: ${reviews}, Rating: ${rating}, Total Discount: ${totalDiscount}tk, Total Price: ${priceAfterDiscount}tk`)
+
     count++
+    totalQty = totalQty+productCount
+    TotalPrice = TotalPrice+priceAfterDiscount
 }
 
+console.log(`Total item purchased: ${totalQty}`)
+console.log(`Total Price: ${TotalPrice}tk`)
 
